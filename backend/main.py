4af14cd32a88179ff7dc19b2a41408253b06a4ad
Giorgiocig <giorgiocignitti@gmail.com>
2026-05-27 11:19:29 +0200

    Add vercel url to allow_origins

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://rag-legal-rho.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
