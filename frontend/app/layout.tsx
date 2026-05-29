14df40836bf72de4369455a94fdb83dbb41587ac
Giorgiocig <giorgiocignitti@gmail.com>
2026-05-29 11:07:09 +0200

    Add sonner to layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <SidebarProvider>{children}</SidebarProvider>
+        <Toaster />
       </body>
     </html>
   );
